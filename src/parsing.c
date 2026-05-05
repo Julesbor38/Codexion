b304df093e5cd47957a411d3a183c45f73a3073c
julesbor38 <jules.bordelier@gmail.com>
2026-05-05 11:05:22 +0200

    patch int min int max

--- src/parsing.c
+++ src/parsing.c
@@ -6,20 +6,34 @@
 /*   By: jbordeli <jbordeli@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/03 19:05:57 by jbordeli          #+#    #+#             */
-/*   Updated: 2026/05/04 16:10:59 by jbordeli         ###   ########.fr       */
+/*   Updated: 2026/05/05 11:02:28 by jbordeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../include/codexion.h"
 
+int ft_strcmp(char *str1, char *str2)
+{
+    int i;
 
+    i = 0;
+    while(str1[i])
+    {
+        if (str1[i] != str2[i])
+            return (str1[i] - str2[i]);
+        i++;
+    }
+    return (0);
+}
 int is_positive_number(char *str)
 {
     long result;
     int i;
 
     i = 0;
+    if ((strlen(str) >= 11 && ft_strcmp(str, "-2147483648") > 0 && str[0] == '-' )|| (strlen(str) >= 10 && ft_strcmp(str, "2147483647") > 0 && str[0] != '-'))
+        return (1);
     while (str[i])
     {
         if (str[i] < '0' || str[i] > '9')
@@ -32,28 +46,28 @@ int is_positive_number(char *str)
     return (0);
     
 }
-int    fill_sim(char **argv, t_sim *sim)
+int    fill_data(char **argv, t_data *data)
 {
-    sim->nb_coders = atoi(argv[1]);
-    sim->time_to_burnout = atoi(argv[2]);
-    sim->time_to_compil = atoi(argv[3]);
-    sim->time_to_debug = atoi(argv[4]);
-    sim->time_to_refactor = atoi(argv[5]);
-    sim->required_compiles = atoi(argv[6]);
-    sim->dongle_cooldown = atoi(argv[7]);
+    data->nb_coders = atoi(argv[1]);
+    data->time_to_burnout = atoi(argv[2]);
+    data->time_to_compil = atoi(argv[3]);
+    data->time_to_debug = atoi(argv[4]);
+    data->time_to_refactor = atoi(argv[5]);
+    data->required_compiles = atoi(argv[6]);
+    data->dongle_cooldown = atoi(argv[7]);
     if(strcmp(argv[8], "fifo") == 0)
     {
-            sim->scheduler = FIFO;
+            data->scheduler = FIFO;
             return 0;
     }
     else if(strcmp(argv[8], "edf") == 0)
     {
-        sim->scheduler = EDF;
+        data->scheduler = EDF;
         return 0;
     }
     return (1);
 }
-int parse_args(int argc, char **argv, t_sim *sim)
+int parse_args(int argc, char **argv, t_data *data)
 {
     int i;
 
@@ -72,29 +86,29 @@ int parse_args(int argc, char **argv, t_sim *sim)
         };
         i++;
     }
-    return(fill_sim(argv, sim));
+    return(fill_data(argv, data));
 }
 
 int def_args(int argc, char **argv)
 {
-    t_sim sim;
-	if (parse_args(argc, argv, &sim) != 0)
+    t_data data;
+	if (parse_args(argc, argv, &data) != 0)
 	{
 		printf("Parsing failed\n");
 		return (1);
 	}
     
 	printf("Parsing OK\n");
-	printf("nb_coders: %d\n", sim.nb_coders);
-	printf("time_to_burnout: %ld\n", sim.time_to_burnout);
-	printf("time_to_compile: %ld\n", sim.time_to_compil);
-	printf("time_to_debug: %ld\n", sim.time_to_debug);
-	printf("time_to_refactor: %ld\n", sim.time_to_refactor);
-	printf("required_compiles: %d\n", sim.required_compiles);
-	printf("dongle_cooldown: %ld\n", sim.dongle_cooldown);
-	if (sim.scheduler == FIFO)
+	printf("nb_coders: %d\n", data.nb_coders);
+	printf("time_to_burnout: %ld\n", data.time_to_burnout);
+	printf("time_to_compile: %ld\n", data.time_to_compil);
+	printf("time_to_debug: %ld\n", data.time_to_debug);
+	printf("time_to_refactor: %ld\n", data.time_to_refactor);
+	printf("required_compiles: %d\n", data.required_compiles);
+	printf("dongle_cooldown: %ld\n", data.dongle_cooldown);
+	if (data.scheduler == FIFO)
 		printf("scheduler: FIFO\n");
-	else if (sim.scheduler == EDF)
+	else if (data.scheduler == EDF)
 		printf("scheduler: EDF\n");
     else
     {
